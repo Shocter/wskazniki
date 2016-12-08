df98a6738a658e52238d1cfb5b7f9a24b1821574
Mateusz Wojno <Mateusz Wojno@SHOCTER>
2016-12-08 16:22:58 +0100

    new version

--- wskazniki/wskazniki.cpp
+++ wskazniki/wskazniki.cpp
@@ -4,32 +4,23 @@
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
-double z1(struct lista *p)
-{
-	double s = 0;
-	int n = 0;
-	while (p != NULL)
-	{
-		s += p->x;
-		n++;
-		p = p->n;
-	}
-	return s / n;
-}
-
 
 int main()
 {
-	int x = 10;
-	int n = 0;
-	int *wskaznik;
-	wskaznik = &n;
-	int *m = &n;
-	//*m = *m + 1;
-	cout << &x << endl << &n << endl;
-	cout << m << endl << *m << endl;
-	z1();
+	int size;
+	cout << "podaj rozmiar tablicy" << endl;
+	cin >> size;
+	int *tab = new int[size];
 
+	cout << "Wpisz dane do tablicy" << endl;
+	for (int i = 0; i < size; i++)
+		cin >> tab[i];
+
+	cout << "Twoja tablica to" << endl;
+	for (int i = 0; i < size; i++)
+		cout << tab[i] << endl;
+
+	delete[] tab;
 	system("pause");
     return 0;
 }
